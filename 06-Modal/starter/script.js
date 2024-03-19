008add14db0b68e3570f87201a5c383c6ca9f40b
r-stake <96791353+r-stake@users.noreply.github.com>
2024-03-20 00:23:59 +0200

    Completed part 6 - Modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,28 @@
-'use strict';
+"use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+function closeModal() {
+  overlay.classList.add("hidden");
+  modal.classList.add("hidden");
+}
+
+function openModal() {
+  overlay.classList.remove("hidden");
+  modal.classList.remove("hidden");
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
